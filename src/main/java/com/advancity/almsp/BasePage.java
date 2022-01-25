9972d130a9be45245fc391829f92bb400887069d
bedirhan ergün <bedirhan1029@gmail.com>
2022-01-25 21:20:31 +0300

    project init

---
+++ src/main/java/com/advancity/almsp/BasePage.java
@@ -0,0 +1,30 @@
+package com.advancity.almsp;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class BasePage {
+    protected WebDriver driver ;
+    String baseUrl = "https://staging.almscloud.com/almsp";
+
+    public BasePage(WebDriver driver){
+        this.driver = driver ;
+    }
+
+    public WebElement find(By locator){
+        return driver.findElement(locator);
+    }
+
+    public void click(By locator){
+        find(locator).click();
+    }
+
+    public void type(By locator , String text){
+        find(locator).sendKeys(text);
+    }
+
+    public Boolean isSelected(By locator){
+        return find(locator).isSelected();
+    }
+}
